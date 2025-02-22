968a39b18acc10e33f5eda19f03b2e3f7c17e697
Ernesto Pang <ernestopang513@hotmail.com>
2025-02-22 00:19:51 -0600

    Time function ok

--- src/PomodoroApp.tsx
+++ src/PomodoroApp.tsx
@@ -1,15 +1,39 @@
 
+import { useEffect, useState } from "react"
 import { Pressable, StyleSheet, Text, View } from "react-native"
 
 export const PomodoroApp = () => {
+
+  const [time, setTime] = useState(60*25);
+
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval: ReturnType<typeof setInterval> | null;
+    
+    if (isRunning && time > 0 ) {
+
+      interval = setInterval(() => {
+        setTime((prevTime) => (prevTime > 0 ? prevTime - 1 : 0))
+      }, 1000)
+    } 
+
+    return () => {
+      if (interval) clearInterval(interval)
+    }
+  }, [isRunning, time])
+  
   
   return (
     <View style={{flex: 1, backgroundColor: '#BA4949', justifyContent: 'center', alignItems: 'center'}}>
       <Text style={styles.title}>Pomodoro</Text>
       <View style={styles.card} >
-        <Text style={styles.timer}>25:00</Text>
-        <Pressable style = {{backgroundColor: 'white', padding: 20, borderRadius: 5, alignItems: 'center'}} onPress={()=>{ return console.log('Native')}}>
-          <Text style={{color: '#BA4949', fontSize: 30, fontWeight: '800'}}>START</Text>
+        <Text style={styles.timer}>{Math.floor(time/60)}:{String(time%60).padStart(2, '0')}</Text>
+        <Pressable 
+          style = {{backgroundColor: 'white', padding: 20, borderRadius: 5, alignItems: 'center'}} 
+          onPress={()=>{ setIsRunning(!isRunning)}}
+        >
+          <Text style={{color: '#BA4949', fontSize: 30, fontWeight: '800'}}>{isRunning? "PAUSE" : 'START'}</Text>
         </Pressable>
       </View>
     </View>
